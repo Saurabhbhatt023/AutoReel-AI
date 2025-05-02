416a161893f283fbc286e74f7e0473893d43b0dd
Saurabh <saurabhbhatt023@gmail.com>
2025-05-02 21:35:22 +0530

    adding and font

---
+++ app/provider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+const Provider = ({children}) => {
+  return (
+    <div>
+
+        <NextThemesProvider
+           attribute= "class"
+           defaultTheme='dark'
+           enableSystem
+           disableTransitionOnChange
+         >
+        {children}
+        </NextThemesProvider>
+    </div>
+  )
+}
+
+export default Provider
