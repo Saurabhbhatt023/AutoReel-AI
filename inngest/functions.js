038b939d9730c15ff0b6302d1de172b5735f279d
Saurabh <saurabhbhatt023@gmail.com>
2025-05-06 21:24:22 +0530

    adding the Generateaudio file to inngest function

--- inngest/functions.js
+++ inngest/functions.js
@@ -8,3 +8,37 @@ export const helloWorld = inngest.createFunction(
     return { message: `Hello ${event.data.email}!` };
   },
 );
+
+const BASE_URL = 'https://aigurulab.tech';
+
+export const GenerateVideoData = inngest.createFunction( 
+  { id: "generate-video-data" },
+  { event: "generate-video-data" },
+  async ({ event, step }) => {
+    const { script, title, caption, videoStyle, voice } = event?.data;
+
+    const GenerateAudioFile = await step.run(
+      "GenerateAudioFile",
+      async () => {
+        const result = await axios.post(
+          BASE_URL + '/api/text-to-speech',
+          {
+            input: script,
+            voice: voice,
+          },
+          {
+            headers: {
+              'x-api-key': process.env.NEXT_PUBLIC_AIGURULAB_API_KEY,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        console.log(result.data.audio); // Output Result: Audio Mp3 Url
+        return result.data.audio;
+      }
+    );
+    return GenerateAudioFile
+    // (You may want to return or use `GenerateAudioFile` here.)
+  }
+
+
